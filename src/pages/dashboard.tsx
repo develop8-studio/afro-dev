d5629732b21beb74c63381e5530525c4c106e05b
dev8 <yuka@MacBook-Air.local>
2024-06-05 01:26:37 +0900

    Update dashboard.tsx

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -50,7 +50,7 @@ export default function DashboardPage() {
   return (
     <div className="flex min-h-screen w-full flex-col">
       <Head>
-        <title>Dashboard -Nook.to</title>
+        <title>Dashboard -Afro.dev</title>
       </Head>
       <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
         <Header current="dashboard" />
