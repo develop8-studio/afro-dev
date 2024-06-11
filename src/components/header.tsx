156145fbbb3478e5ceeeb1b547170611df6b293b
dev8 <yuka@MacBook-Air.local>
2024-06-12 00:31:20 +0900

    Update Header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -34,37 +34,48 @@ export default function Header({ current }: HeaderProps) {
 
     return (
         <header className="sticky top-0 flex h-16 items-center gap-5 bg-background px-5  z-10">
-            <nav className="hidden flex-col gap-5 md:flex md:flex-row md:items-center">
+            <nav className="hidden flex-col gap-5 md:flex md:flex-row md:items-center mr-1">
                 <Link href="/" className="">
                     <Image src="/afro-black-text.png" alt="" width={100} height={100} className="w-40 lg:w-20 block dark:hidden" />
                     <Image src="/afro-white-text.png" alt="" width={100} height={100} className="w-40 lg:w-20 hidden dark:block" />
                 </Link>
             </nav>
             <nav className="hidden flex-col gap-5 md:flex md:flex-row md:items-center md:text-sm">
-                <Link href="/tools" className={`${current === "tools" ? 'text-foreground dark:text-black bg-emerald-100 dark:bg-slate-200' : 'text-muted-foreground'} p-2.5 rounded-md transition-colors hover:text-foreground`}>
+                <Link href="/tools" className={`${current === "tools" ? 'text-foreground bg-slate-100 dark:bg-slate-800' : 'text-muted-foreground'} p-2.5 rounded-md transition-colors hover:text-foreground`}>
                     Tools
                 </Link>
-                <Link href="/threads" className={`${current === "threads" ? 'text-foreground dark:text-black bg-emerald-100 dark:bg-slate-200' : 'text-muted-foreground'} p-2.5 rounded-md transition-colors hover:text-foreground`}>
+                <Link href="/threads" className={`${current === "threads" ? 'text-foreground bg-slate-100 dark:bg-slate-800' : 'text-muted-foreground'} p-2.5 rounded-md transition-colors hover:text-foreground`}>
                     Threads
                 </Link>
-                <Link href="/settings" className={`${current === "settings" ? 'text-foreground dark:text-black bg-emerald-100 dark:bg-slate-200' : 'text-muted-foreground'} p-2.5 rounded-md transition-colors hover:text-foreground`}>
+                <Link href="/settings" className={`${current === "settings" ? 'text-foreground bg-slate-100 dark:bg-slate-800' : 'text-muted-foreground'} p-2.5 rounded-md transition-colors hover:text-foreground`}>
                     Settings
                 </Link>
+                {/* <Link href="/tools" className={`${current === "tools" ? 'text-foreground' : 'text-muted-foreground'}`}>
+                    Tools
+                </Link>
+                <Link href="/threads" className={`${current === "threads" ? 'text-foreground' : 'text-muted-foreground'}`}>
+                    Threads
+                </Link>
+                <Link href="/settings" className={`${current === "settings" ? 'text-foreground' : 'text-muted-foreground'}`}>
+                    Settings
+                </Link> */}
             </nav>
             <div className="w-full">
                 <MobileSheet current={current} />
             </div>
             <SearchMenu />
             {user ? (
-                <UserMenu />
+                <>
+                    <UserMenu />
+                    <Button className="hidden md:flex bg-blue-500 hover:bg-blue-600 dark:text-white" asChild>
+                        <Link href="/threads">Threads</Link>
+                    </Button>
+                </>
             ) : (
                 <Button className="h-[35px]" asChild>
                     <Link href="/login">Login</Link>
                 </Button>
             )}
-            <Button className="hidden md:flex bg-blue-500 hover:bg-blue-600 dark:text-white" asChild>
-                <Link href="/threads">Threads</Link>
-            </Button>
         </header>
     )
 }
