195d7b6c95f375f984b9c820916d871f93072d47
dev8 <yuka@MacBook-Air.local>
2024-06-13 00:51:50 +0900

    Update CodeShare

--- src/components/CodeShare.tsx
+++ src/components/CodeShare.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { collection, doc, addDoc, updateDoc, query, orderBy, onSnapshot, serverTimestamp, getDoc, setDoc } from 'firebase/firestore';
+import { collection, doc, addDoc, updateDoc, query, orderBy, onSnapshot, serverTimestamp, getDoc, setDoc, deleteDoc } from 'firebase/firestore';
 import { db, auth } from '@/firebase/firebaseConfig';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
@@ -47,7 +47,28 @@ const highlightLanguages = [
         value: "typescript",
         label: "TypeScript",
     },
+    {
+        value: "python",
+        label: "Python",
+    },
+    {
+        value: "ruby",
+        label: "Ruby",
+    },
+    {
+        value: "swift",
+        label: "Swift",
+    },
+    {
+        value: "rust",
+        label: "Rust",
+    },
+    {
+        value: "go",
+        label: "Golang",
+    },
 ]
+highlightLanguages.sort((a, b) => a.label.localeCompare(b.label));
 
 const CodeShare: React.FC = () => {
     const [user, setUser] = useState<User | null>(null);
@@ -55,7 +76,7 @@ const CodeShare: React.FC = () => {
     const [description, setDescription] = useState('');
     const [codeSnippets, setCodeSnippets] = useState<CodeSnippet[]>([]);
     const [userIcons, setUserIcons] = useState<{ [userId: string]: string }>({});
-    const [userLikes, setUserLikes] = useState<{ [snippetId: string]: number }>({});
+    const [userLikes, setUserLikes] = useState<{ [snippetId: string]: boolean }>({});
     const [language, setLanguage] = useState<string>('');
 
     useEffect(() => {
@@ -100,7 +121,7 @@ const CodeShare: React.FC = () => {
         if (likeDoc.exists()) {
             setUserLikes(prevState => ({
                 ...prevState,
-                [snippetId]: likeDoc.data().count
+                [snippetId]: true
             }));
         }
     };
@@ -131,34 +152,35 @@ const CodeShare: React.FC = () => {
         const likeDocRef = doc(db, 'likes', `${user.uid}_${snippetId}`);
         const likeDoc = await getDoc(likeDocRef);
 
-        if (likeDoc.exists() && likeDoc.data().count >= 1) {
-            return;
-        }
-
         const snippetRef = doc(db, 'codes', snippetId);
         const snippetDoc = await getDoc(snippetRef);
 
         if (snippetDoc.exists()) {
             const currentLikes = snippetDoc.data().likes || 0;
 
-            await updateDoc(snippetRef, {
-                likes: currentLikes + 1
-            });
-
             if (likeDoc.exists()) {
-                await updateDoc(likeDocRef, {
-                    count: likeDoc.data().count + 1
+                // Unlike the snippet
+                await deleteDoc(likeDocRef);
+                await updateDoc(snippetRef, {
+                    likes: currentLikes - 1
                 });
+                setUserLikes(prevState => ({
+                    ...prevState,
+                    [snippetId]: false
+                }));
             } else {
+                // Like the snippet
                 await setDoc(likeDocRef, {
                     count: 1
                 });
+                await updateDoc(snippetRef, {
+                    likes: currentLikes + 1
+                });
+                setUserLikes(prevState => ({
+                    ...prevState,
+                    [snippetId]: true
+                }));
             }
-
-            setUserLikes(prevState => ({
-                ...prevState,
-                [snippetId]: (prevState[snippetId] || 0) + 1
-            }));
         }
     };
 
@@ -167,8 +189,9 @@ const CodeShare: React.FC = () => {
     }
 
     return (
-            <div className="flex flex-col items-center">
-                <div className="contents sm:flex w-full">
+        <>
+            <div className="flex flex-col h-full">
+                <div className="contents sm:flex">
                     <Select
                         value={language}
                         onValueChange={(value) => setLanguage(value)}
@@ -184,45 +207,47 @@ const CodeShare: React.FC = () => {
                             ))}
                         </SelectContent>
                     </Select>
-                    <Input value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Enter description..." className="mb-3"/>
+                    <div className="flex w-full">
+                        <Input value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Enter description..." className="mb-3"/>
+                        <Button onClick={shareCode} className="ml-3">Share<IoIosSend className="ml-[5px] text-lg" /></Button>
+                    </div>
                 </div>
                 <Textarea value={code} onChange={(e) => setCode(e.target.value)} placeholder="Enter your code..." className="mb-3"/>
-                <Button onClick={shareCode}>Share<IoIosSend className="ml-[5px] text-lg" /></Button>
-                <div className="mt-10 flex-1 space-y-[15px] w-full">
-                    {codeSnippets.map(snippet => (
-                        <Card key={snippet.id} className="px-5 py-[17.5px] shadow-none">
-                            <div className="flex items-center mb-2.5">
-                                {userIcons[snippet.userId] && (
-                                    <img src={userIcons[snippet.userId]} alt="User Icon" className="w-10 h-10 rounded-full mr-2.5 border" />
-                                )}
-                                <span className="font-bold">{snippet.userName}</span>
-                                <span className="ml-2.5 text-xs text-slate-500 font-light">
-                                    {snippet.timestamp ? new Date(snippet.timestamp.toDate()).toLocaleString() : 'No timestamp'}
-                                </span>
-                            </div>
-                            <div className="mb-2.5 text-sm">{snippet.description}</div>
-                            {/* <CodeBlock language='javascript'><pre className="bg-slate-100 dark:bg-slate-900 p-2.5 rounded-md text-sm whitespace-pre-wrap">{snippet.code}</pre></CodeBlock> */}
-                            {snippet.language && (
-                                <>
-                                    <CodeBlock language={snippet.language}>
-                                        <pre className="bg-slate-100 dark:bg-slate-900 p-2.5 rounded-md text-sm whitespace-pre-wrap">{snippet.code}</pre>
-                                    </CodeBlock>
-                                    <pre className="hidden dark:block bg-slate-100 dark:bg-slate-900 p-2.5 text-sm whitespace-pre-wrap">{snippet.code}</pre>
-                                </>
-                            )}
-                            {!snippet.language && (
-                                <pre className="bg-[#F3F3F3] dark:bg-slate-900 p-2.5 text-sm whitespace-pre-wrap">{snippet.code}</pre>
-                            )}
-                            <div className="flex items-center mt-2.5 pt-2.5 pb-[7.5px]">
-                                <Button onClick={() => likeSnippet(snippet.id)} className="bg-transparent hover:bg-transparent h-0 p-0">
-                                    <FaHeart className={`text-lg mr-[10px] transition-all ${userLikes[snippet.id] ? 'text-red-500' : 'text-slate-300'} hover:text-red-500`} />
-                                </Button>
-                                <span className="text-sm text-slate-500">{snippet.likes}</span>
-                            </div>
-                        </Card>
-                    ))}
-                </div>
             </div>
+            <div className="mt-5 flex-1 space-y-[15px]">
+                {codeSnippets.map(snippet => (
+                    <Card key={snippet.id} className="px-5 py-[17.5px] shadow-none">
+                        <div className="flex items-center mb-2.5">
+                            {userIcons[snippet.userId] && (
+                                <img src={userIcons[snippet.userId]} alt="User Icon" className="w-10 h-10 rounded-full mr-2.5 border" />
+                            )}
+                            <span className="font-bold">{snippet.userName}</span>
+                            <span className="ml-2.5 text-xs text-slate-500 font-light">
+                                {snippet.timestamp ? new Date(snippet.timestamp.toDate()).toLocaleString() : 'No timestamp'}
+                            </span>
+                        </div>
+                        <div className="mb-2.5 text-sm">{snippet.description}</div>
+                        {snippet.language && (
+                            <>
+                                <CodeBlock language={snippet.language}>
+                                    <pre className="bg-slate-100 dark:bg-slate-900 p-2.5 rounded-md text-sm whitespace-pre-wrap">{snippet.code}</pre>
+                                </CodeBlock>
+                                <pre className="hidden dark:block bg-slate-100 dark:bg-slate-900 p-2.5 text-sm whitespace-pre-wrap">{snippet.code}</pre>
+                            </>
+                        )}
+                        {!snippet.language && (
+                            <pre className="bg-[#F3F3F3] dark:bg-slate-900 p-2.5 text-sm whitespace-pre-wrap">{snippet.code}</pre>
+                        )}
+                        <div className="flex items-center mt-2.5 pt-2.5 pb-[7.5px]">
+                            <Button onClick={() => likeSnippet(snippet.id)} className="bg-transparent hover:bg-transparent h-0 p-0">
+                                <FaHeart className={`text-lg mr-[10px] transition-all ${userLikes[snippet.id] ? 'text-red-500' : 'text-slate-300'}`} />
+                            </Button>
+                            <span className="text-sm text-slate-500">{snippet.likes}</span>
+                        </div>
+                    </Card>
+                ))}
+            </div>
+        </>
     );
 }
 
